9b1f85b63322ae553a6878c2dc86504e626101ce
ColemakDH <a.colton123@gmail.com>
2023-02-24 09:08:56 -0500

    Question 29 project added onto with explanations and images

---
+++ projects/valid.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Casting Variables</title>
+<script>
+function myFunction() {
+    var a = 1;
+    var b = 2;
+    var c = "the answer is " + a + b;
+    alert(c);
+}
+</script>
+</head>
+<body onload="myFunction();">
+</body>
+</html>
