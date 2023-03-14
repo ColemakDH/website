6b3b14cd0c21cdd251aa6d0a51ea6c721f2f5fc3
ColemakDH <a.colton123@gmail.com>
2023-03-14 10:12:37 -0400

    Fixed phrasing and links

--- projects/valid.js
+++ projects/valid.js
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Casting Variables</title>
-<script>
-function myFunction() {
-    var a = 1;
-    var b = 2;
-    var c = "the answer is " + a + b;
-    alert(c);
-}
-</script>
-</head>
-<body onload="myFunction();">
-</body>
-</html>
+function init() {
+    var loc = window.location.href;
+    var HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
+    var CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +
+                          loc + '?profile=css3';
+    document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
+    document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
+  }
+  window.onload = init;
